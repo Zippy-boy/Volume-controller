9a68ca8116d9385cec32cca11877428e84021e67
Zippy-boy <zippy-boy@outlook.com>
2024-03-07 12:59:27 +0000

    yep

--- OnPC/flaskapp/flaskUI.py
+++ OnPC/flaskapp/flaskUI.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flaskwebgui import FlaskUI
 
 app = Flask(__name__)
+input_data = ""
 
 @app.route('/')
 def index_page():
+    global input_data
     a = "hello"
-    return render_template('index.html', a=a)  # Pass 'a' as a parameter
+    hello = 200
+    return render_template('index.html', a=input_data, hello=hello)  # Pass 'a' as a parameter
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    global input_data
+    input_data = request.form['asd']
+    print(input_data)
+    return input_data
 
 if __name__ == '__main__':
     FlaskUI(app=app, server="flask").run()

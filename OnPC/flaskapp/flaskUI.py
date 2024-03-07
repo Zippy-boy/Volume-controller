72f99e0db02315237ca6aa7602230f0521aff368
Zippy-boy <zippy-boy@outlook.com>
2024-03-07 22:23:50 +0000

    REPLACEING ALL ' WITH " FOR JSON NOT WORKING!!!!!!!!!
    GOt volume change ot work thoug

--- OnPC/flaskapp/flaskUI.py
+++ OnPC/flaskapp/flaskUI.py
@@ -6,7 +6,22 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 
 app = Flask(__name__)
-input_data = ""
+with open("sliders.txt", "r") as file:
+    datas = file.read()
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+master_volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+
+def change_volume(app, percentage):
+    print(app, percentage)
+    sessions = AudioUtilities.GetAllSessions()
+    for session in sessions:
+        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+        if session.Process and session.Process.name() == app:
+            volume.SetMasterVolume(percentage, None)
 
 def find_open_apps():
     pythoncom.CoInitialize()  # Initialize the COM library
@@ -19,21 +34,42 @@ def find_open_apps():
 
 @app.route('/')
 def index_page():
-    global input_data
     pythoncom.CoInitialize()  # Initialize the COM library
     apps = find_open_apps()
-    for i, app in enumerate(apps):
-        if app[-4:] == ".exe":
-            apps[i] = app[:-4]
-
-    hello = 200
-    return render_template('index.html', apps=apps, hello=hello)  # Pass 'a' as a parameter
+    with open("sliders.txt", "r") as file:
+        input_data = file.read()
+        input_data = input_data.split("},")
+        for da in input_data:
+            print(da)
+        slider1Apps = input_data[0]
+        print(slider1Apps)
+        slider2Apps = []
+        slider3Apps = []
+        slider4Apps = []
+    return render_template('index.html', apps=apps)  # Pass 'a' as a parameter
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    global input_data
-    input_data = request.form['asd']
-    print(input_data)
+    global datas
+    # read all the insides of the divs with .appBox
+    input_data = request.get_json()
+    for data in input_data:
+        print(data)
+    datas = input_data
+    with open("sliders.txt", "w") as file:
+        file.write(str(datas))
+    return redirect(url_for('index_page'))
+
+@app.route('/change_volume', methods=['POST'])
+def change_volume_route():
+    input_data = request.get_json()
+    slider_index = input_data['slider']
+    percentage = input_data['volume']
+    print(datas[int(slider_index)-1]['apps'])
+    for app in datas[int(slider_index)-1]['apps']:
+        print(app, percentage)
+        change_volume(app, percentage)
+    # print(apps)
     return redirect(url_for('index_page'))
 
 if __name__ == '__main__':

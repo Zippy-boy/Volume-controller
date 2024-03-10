65223719b75f8f1c256622e27f8bbd4b353642a8
Zippy-boy <zippy-boy@outlook.com>
2024-03-10 21:31:36 +0000

    JSON DUMPS SAVES MY LIFE

--- OnPC/flaskapp/flaskUI.py
+++ OnPC/flaskapp/flaskUI.py
@@ -4,6 +4,7 @@ from flaskwebgui import FlaskUI
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume, ISimpleAudioVolume
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
+import json
 
 app = Flask(__name__)
 with open("sliders.txt", "r") as file:
@@ -40,9 +41,10 @@ def index_page():
         input_data = file.read()
         input_data = input_data.split("},")
         for da in input_data:
-            print(da)
+            # print(da)
+            pass
         slider1Apps = input_data[0]
-        print(slider1Apps)
+        # print(slider1Apps)
         slider2Apps = []
         slider3Apps = []
         slider4Apps = []
@@ -50,14 +52,25 @@ def index_page():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    global datas
-    # read all the insides of the divs with .appBox
     input_data = request.get_json()
-    for data in input_data:
-        print(data)
-    datas = input_data
+    newSliderDict = []
+    # print(input_data[0])
+    for slider in input_data:
+        apps = slider['apps']
+        slider = slider['slider']
+        newApps = []
+        # print(slider, apps)
+        for app in apps:
+            newApps.append(app.replace("\n", "").replace(" ", ""))
+        # print(newApps)
+        newSliderDict.append({
+            "slider": slider,
+            "apps": newApps
+        })
+    
+    print(json.dumps(newSliderDict))
     with open("sliders.txt", "w") as file:
-        file.write(str(datas))
+        file.write(str(json.dumps(newSliderDict)))
     return redirect(url_for('index_page'))
 
 @app.route('/change_volume', methods=['POST'])
@@ -65,12 +78,16 @@ def change_volume_route():
     input_data = request.get_json()
     slider_index = input_data['slider']
     percentage = input_data['volume']
-    print(datas[int(slider_index)-1]['apps'])
-    for app in datas[int(slider_index)-1]['apps']:
-        print(app, percentage)
-        change_volume(app, percentage)
-    # print(apps)
-    return redirect(url_for('index_page'))
+    with open("sliders.txt", "r") as file:
+        file = file.readline().replace("]", "").replace("[", "").split("},")
+        print(file[0])
+        
+        for app in file[0]["slider": slider_index]["apps"]:
+
+            print(app, percentage)
+            # change_volume(app, percentage)
+        # print(apps)
+        return redirect(url_for('index_page'))
 
 if __name__ == '__main__':
     FlaskUI(app=app, server="flask").run()

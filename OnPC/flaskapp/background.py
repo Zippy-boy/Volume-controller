d214bccb4eb6556f19b028f6d0a124d95d494117
Zippy-boy <zippy-boy@outlook.com>
2024-03-14 08:41:16 +0000

    Background kinda works

--- OnPC/flaskapp/background.py
+++ OnPC/flaskapp/background.py
@@ -2,14 +2,42 @@ from __future__ import print_function
 try:
     from ctypes import cast, POINTER
     from comtypes import CLSCTX_ALL
-    from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+    from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume, ISimpleAudioVolume
     import serial
     import pystray
     from PIL import Image
     import os
     import subprocess
     import json
+    import threading
+    from threading import Event
     import serial.tools.list_ports
+    import pythoncom
+    import numpy as np
+
+    def change_master_volume(volume_level):
+        with open("minimum_value.txt", "r") as f:
+            lowest_volume_limit = float(f.read())
+
+        volume_level = asd(int(volume_level), 100, 0, lowest_volume_limit, 0)
+        # print(volume_level)
+
+        print(f"soejf h0ipw: {(np.emath.logn(1.07346, volume_level)) - 58.5582}")
+        mate_idk = (np.emath.logn(1.07346, volume_level)) - 58.5582
+        mate_idk = np.clip(mate_idk, lowest_volume_limit, 0)
+
+        # print(f"Master volume raw: {mate_idk}")
+
+        master_volume.SetMasterVolumeLevel(int(mate_idk), None)
+
+    def change_volume(app, percentage):
+        percentage = asd(int(percentage), 0, 100, 0, 1)
+        print(app, percentage)
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+            if session.Process and session.Process.name() == app:
+                volume.SetMasterVolume(percentage, None)
 
     def getAudionoPort():
         ports = list(serial.tools.list_ports.comports())
@@ -25,7 +53,12 @@ try:
     def getApps(slider):
         with open("C:/Users/Zippy/Documents/GitHub/Volume-controller/OnPC/flaskapp/sliders.json") as file:
             file = json.load(file)
-            apps = file[slider]["apps"]
+            print(slider)
+            print(file[0])
+            print(file[1])
+            print(file[2])
+            print(file[3])
+            apps = file[int(slider-2)]["apps"]
         
         return apps
 
@@ -34,7 +67,13 @@ try:
         subprocess.Popen(ui_exe_path)
 
     def quit_action(icon, item):
+        event.set()
         icon.stop()
+    
+    def asd(num, in_min, in_max, out_min, out_max):
+        return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+    event = Event()
 
     image = Image.open("C:/Users/Zippy/Documents/GitHub/Volume-controller/OnPC/flaskapp/static/icon.png")
 
@@ -47,9 +86,69 @@ try:
 
     # open serial port
     ser = serial.Serial(str(getAudionoPort()))
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    master_volume = cast(interface, POINTER(IAudioEndpointVolume))
+
     # print(ser.readline().decode('utf-8').strip())
-    nob1_pre = ser.readline().decode('utf-8').strip().split(',')
-    print(nob1_pre)
+    def read_serial_data(event: Event):
+        pythoncom.CoInitialize()
+        nob1_pre = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[0])
+        nob1 = nob1_pre
+        nob2_pre = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[1])
+        nob2 = nob2_pre
+        nob3_pre = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[2])
+        nob3 = nob3_pre
+        nob4_pre = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[3])
+        nob4 = nob4_pre
+        nob5_pre = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[4])
+        nob5 = nob5_pre
+
+        while True:
+            if event.is_set():
+                break
+
+            if nob1 != nob1_pre:
+                change_master_volume(int(nob1))
+                nob1_pre = nob1
+
+            if nob2 != nob2_pre and nob2 != nob2_pre+1 and nob2 != nob2_pre-1:
+                nob2_pre = nob2
+                apps = getApps(2)
+                for app in apps:
+                    change_volume(app, nob2)
+
+            if nob3 != nob3_pre and nob3 != nob3_pre+1 and nob3 != nob3_pre-1:
+                nob3_pre = nob3
+                apps = getApps(3)
+                for app in apps:
+                    change_volume(app, nob3)
+
+            if nob4 != nob4_pre and nob4 != nob4_pre+1 and nob4 != nob4_pre-1:
+                nob4_pre = nob4
+                apps = getApps(4)
+                for app in apps:
+                    change_volume(app, nob4)
+
+            if nob5 != nob5_pre and nob5 != nob5_pre+1 and nob5 != nob5_pre-1:
+                nob5_pre = nob5
+                apps = getApps(5)
+                for app in apps:
+                    change_volume(app, nob5)
+            
+
+            nob1 = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[0])
+            nob2 = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[1])
+            nob3 = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[2])
+            nob4 = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[3])
+            nob5 = int(ser.readline().decode('utf-8', 'ignore').strip().split(',')[4])
+
+            print(ser.readline().decode('utf-8', 'ignore').strip().split(','))
+
+            
+
+    # Start the thread
+    threading.Thread(target=read_serial_data, args=(event,)).start()
 
 
     icon.menu = pystray.Menu(*menu)
